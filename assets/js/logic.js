3c652d6b856792e045bf31ef5c7ef89ebd10ee85
jmacker101 <jm470t@gmail.com>
2024-04-09 21:55:30 -0400

    need help transfering css dark mnode light mode to javascript

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,4 +4,11 @@
 // swap mode
 // function to apply the css changes based off current mode
 
-
+$(document).ready(function(){
+    setTimeout(function(){
+      $('#light-switch').trigger('click');
+    },500);
+    setTimeout(function(){
+      $('#light-switch').trigger('click');
+    },1000)
+  });
